970a5a71678d738113138b9b6a2cafe123c75513
Elsayed-Elpna <said.elpna@yahoo.com>
2025-12-13 21:38:24 +0200

    make layout

--- GreenBite-frontend/src/App.jsx
+++ GreenBite-frontend/src/App.jsx
@@ -1,23 +1,29 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
-import DashBoardPage from "./pages/DashBoardPage";
-import Header from "./components/Header";
-import PrivateRoute from "./utils/PrivateRoute";
+import DashBoardPage from "./pages/DashBoardPages/DashBoardHome";
+import DashboardLayout from "./layouts/DashboardLayout";
+// import PrivateRoute from "./utils/PrivateRoute";
+
+import Testoo from "./pages/DashBoardPages/testoo";
+import Testooo from "./pages/DashBoardPages/testooo";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} exact />
           <Route path="/login" element={<LoginPage />} />
-          <Route element={<PrivateRoute />}>
-            <Route path="/dashbaord" element={<DashBoardPage />} />
+          {/* <Route element={<PrivateRoute />}></Route> */}
+          <Route path="/dashboard" element={<DashboardLayout />}>
+            <Route index element={<DashBoardPage />} />
+            <Route path="testoo" element={<Testoo />} />
+            <Route path="testooo" element={<Testooo />} />
           </Route>
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ GreenBite-frontend/src/pages/DashBoardPages/DashBoardHome.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashBoardHome = () => {
+  return <div>testststs from home</div>;
+};
+
+export default DashBoardHome;
