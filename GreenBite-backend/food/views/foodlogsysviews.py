b5a6e93631b896ba04d256b495c904ac084d2f26
FatmaMohamedIsmail <fmohamed440@gmail.com>
2026-01-04 17:44:36 +0200

    Updated meals UI

--- GreenBite-backend/food/views/foodlogsysviews.py
+++ GreenBite-backend/food/views/foodlogsysviews.py
@@ -7,7 +7,7 @@ from rest_framework import status
 from ..models import FoodLogSys
 from ..serializers import FoodLogSysSerializer
 from ..filters import FoodLogFilter 
-from ..utils.caching import bump_list_version, detail_key, list_key
+from ..utils.caching import bump_list_version, detail_key, list_key, invalidate_cache
 from ..pagination import FoodLogPagination
 
 NAMESPACE = "foodlog"
@@ -61,7 +61,7 @@ def food_log_list_create(request):
             paginated_queryset, many=True
         )
         response = paginator.get_paginated_response(serializer.data)
-        cache.set(cache_key, response.data, timeout= 60*60*24)
+        cache.set(cache_key, response.data, timeout= 60*5)
         return response
 
     elif request.method == 'POST':
@@ -90,7 +90,7 @@ def food_log_detail(request, pk):
             return Response(cached, status=status.HTTP_200_OK)
         
         data = FoodLogSysSerializer(food_log).data
-        cache.set(ck, data, timeout= 60*60*24) 
+        cache.set(ck, data, timeout= 60*5) 
         return Response(data, status=status.HTTP_200_OK)
     
     elif request.method in ['PUT', 'PATCH']:
@@ -102,6 +102,8 @@ def food_log_detail(request, pk):
 
             cache.delete(detail_key(NAMESPACE, request.user.id, pk))
             bump_list_version(NAMESPACE, request.user.id)
+            if food_log.meal:
+                invalidate_cache("meals", request.user.id, detail_id=food_log.meal.id)
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
@@ -134,40 +136,44 @@ def food_log_detail(request, pk):
                 "updated_at"
             ])
 
-        
+
+
         cache.delete(detail_key(NAMESPACE, request.user.id, pk))
         bump_list_version(NAMESPACE, request.user.id)
+        if meal:
+            invalidate_cache("meals", request.user.id, detail_id=meal.id)
+
         return Response(
             {"message": "Food log deleted successfully"},
             status=status.HTTP_204_NO_CONTENT
         )
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def food_log_create(request):
-    """
-    Create a new food log entry.
-    """
-    serializer = FoodLogSysSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save(user=request.user)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+# def food_log_create(request):
+#     """
+#     Create a new food log entry.
+#     """
+#     serializer = FoodLogSysSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save(user=request.user)
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['PUT', 'PATCH'])
-@permission_classes([IsAuthenticated])
-def food_log_update(request, pk):
-    """
-    Update an existing food log entry.
-    """
-    food_log = get_object_or_404(FoodLogSys, pk=pk, user=request.user)
-    partial = request.method == 'PATCH'
-    serializer = FoodLogSysSerializer(food_log, data=request.data, partial=partial)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['PUT', 'PATCH'])
+# @permission_classes([IsAuthenticated])
+# def food_log_update(request, pk):
+#     """
+#     Update an existing food log entry.
+#     """
+#     food_log = get_object_or_404(FoodLogSys, pk=pk, user=request.user)
+#     partial = request.method == 'PATCH'
+#     serializer = FoodLogSysSerializer(food_log, data=request.data, partial=partial)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # @api_view(['DELETE'])
